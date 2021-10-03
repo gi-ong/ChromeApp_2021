80e71fe71f0e5d940ea3cfae5de7f5b1da9ef4d5
ongidea <58909989+gi-ong@users.noreply.github.com>
2021-10-04 03:01:05 +0900

    fin 211004

--- js/greeting.js
+++ js/greeting.js
@@ -1,6 +1,8 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const helloUser = greeting.querySelector("h1");
+const logOutBtn = greeting.querySelector(".clear-btn");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -13,9 +15,19 @@ function onLoginSubmit(e) {
   paintGreetings(username);
 }
 
+function logOutUser() {
+  helloUser.innerText = "";
+  loginInput.value = "";
+  localStorage.clear();
+  greeting.classList.add(HIDDEN_CLASSNAME);
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+}
+
 function paintGreetings(username) {
-  greeting.innerText = `Hello ${username}`;
+  helloUser.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
+  logOutBtn.classList.remove(HIDDEN_CLASSNAME);
+  logOutBtn.addEventListener("click", logOutUser);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
