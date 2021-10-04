578dc6a84e8c77b101a761a720db45d3e054cbba
ongidea <58909989+gi-ong@users.noreply.github.com>
2021-10-04 21:00:46 +0900

    add css+js style

--- js/pageChange.js
+++ js/pageChange.js
@@ -1,8 +1,17 @@
 const changePage = document.querySelector(".change-page-icon");
 const pageWrap = document.querySelector(".flip-btn");
+const pageOne = pageWrap.querySelector(".page-one");
+const pageTwo = pageWrap.querySelector(".page-two");
 
 function rotatePage() {
   pageWrap.classList.toggle("rotate-page");
+  if (pageWrap.classList.contains("rotate-page")) {
+    pageTwo.style.opacity = "1";
+    pageOne.style.opacity = "0.3";
+  } else {
+    pageOne.style.opacity = "1";
+    pageTwo.style.opacity = "0.3";
+  }
 }
 
 changePage.addEventListener("click", rotatePage);
